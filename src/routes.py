96fe33cb85a68272dcf86bf7562b40ac7631cc55
Nicolas Solans <nico.solans.drc@gmail.com>
2024-11-04 19:26:02 -0300

    Routes
    CRUD delete y post completos

--- src/routes.py
+++ src/routes.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template, redirect, url_for, request, jsonif
 from . import db
 from .models import Photo
 from .schemas import PhotoCreateSchema
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Dict
+#import wikipedia
 
 photo_bp = Blueprint('photo_bp', __name__)
 
@@ -28,12 +29,17 @@ def create_photo() -> str:
     except ValueError as e:
         return jsonify({"error": e}), 400
 
-    new_photo = Photo(title=data['title'],
-                        description=data['description'],
+    new_photo = Photo(title=data['title'].title(),
+                        description=data['description'].title(),
                         image=str(data['image']))
     
     db.session.add(new_photo)
     db.session.commit()
+
+    # try:
+    #     wikidata = str(wikipedia.summary(f"{new_photo.title} {new_photo.description}", sentences=1))
+    # except:
+    #     wikidata = "Informacion no encontrada"
 
     return render_template('base.html', photo=new_photo)
 
@@ -42,10 +48,11 @@ def create_photo() -> str:
 @photo_bp.route('/photos/<int:photo_id>/edit')
 def edit_photo_form(photo_id: int) -> str:
     photo: Photo = Photo.query.get_or_404(photo_id)
-    return render_template('base.html', photo=photo)
+    return render_template('photo_form.html', photo=photo)
 
 
-@photo_bp.route('/photo/<int:photo_id>', methods=['DELTE'])
+#Eliminar fotos
+@photo_bp.route('/photos/<int:photo_id>', methods=['DELETE'])
 def delete_photo(photo_id: int) -> str:
     photo: Photo = Photo.query.get_or_404(photo_id)
 
@@ -54,5 +61,31 @@ def delete_photo(photo_id: int) -> str:
 
     photos: List[Tuple[Any]] = Photo.query.all()
 
-    return render_template('index.html', photos=photos)
+    return redirect(url_for('photo_bp.index', photos=photos))
     
+
+#Actualizar foto
+@photo_bp.route('/photos/<int:photo_id>/', methods=['GET', 'PUT', 'POST'])
+def update_photo(photo_id: int) -> str:
+    photo: Photo = Photo.query.get_or_404(photo_id)
+
+    if request.method == 'POST':
+        data: Dict[str, str] = request.form
+
+        try:
+            photo_data: PhotoCreateSchema = PhotoCreateSchema(title=data['title'],
+                                       description=data['description'],
+                                       image=str(data['image']))
+        
+        except ValueError as e:
+            return jsonify({"error": e}), 400
+        
+        photo.title = photo_data.title
+        photo.description = photo_data.description
+        photo.image = photo_data.image
+
+        db.session.commit()
+
+        return redirect(url_for('photo_bp.update_photo', photo_id=photo.id))
+    
+    return render_template('base.html', photo=photo)
